330424eb5a667d9299b9b40d9c7f8b25aec1acdb
tapiaw38 <tapiaw38@gmail.com>
2021-10-22 17:11:51 -0300

    changed confing

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,7 @@
-set -o errexit
-set -o pipefail
-set -o nounset
+#!/bin/bash
 
 
 python /app/manage.py collectstatic --noinput
-/usr/local/bin/gunicorn config.wsgi --bind 0.0.0.0:5000 --chdir=/app
 
 echo "$@"
 exec "$@"
